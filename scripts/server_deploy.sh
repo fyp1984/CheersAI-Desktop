68496932f6739861d0e5b53192743baa7f7b303b
西北人 <fyp1984@yeah.net>
2026-03-13 23:33:42 +0800

    feat(deploy): update default branch to branch2B_v1.1

--- scripts/server_deploy.sh
+++ scripts/server_deploy.sh
@@ -22,8 +22,8 @@ log() {
 }
 
 # 交互式确认：Git 分支
-read -p "请输入要部署的 Git 分支 [默认: branch2B_v1.0]: " INPUT_BRANCH
-BRANCH=${INPUT_BRANCH:-branch2B_v1.0}
+read -p "请输入要部署的 Git 分支 [默认: branch2B_v1.1]: " INPUT_BRANCH
+BRANCH=${INPUT_BRANCH:-branch2B_v1.1}
 
 log "=== 开始部署流程 (分支: $BRANCH) ==="
 
